c69e4a0e19da96685a3debab3a8ffd28368f7ea1
nkey-ops <terrythebear746@gmail.com>
2024-07-17 07:31:10 +0400

    Initial commit

---
+++ i3_scratchpad_show_or_create.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# try showing the scratchpad window
+if ! i3-msg scratchpad show; then
+    # if there is no such window...
+
+    # # Wait for the next window event.
+    i3-msg -t subscribe  '[ "window" ]' &
+    # # launch the application.
+    eval "terminator" 
+    
+    # # Move it to the scratchpad workspace
+    i3-msg move scratchpad
+fi
